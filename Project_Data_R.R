4727713eabb158446e6d0135890fe05c9de6ccac
Indycoder <vmpharm@gmail.com>
2015-05-28 22:50:17 +0100

    V0.1
    changes made

---
+++ Project_Data_R.R
@@ -0,0 +1,14 @@
+# Project_data <- read.csv("~/Desktop/dissertation project/Project_data.csv")
+# View(Project_data)
+# Map.Table<-data.frame(levels(Project_data$Category))
+# print(names(Project_data))
+# write.csv(Project_data, file="Project_data.csv", row.names=FALSE, quote=TRUE)
+# install.packages(c("lubridate","qcc","knitr"))
+# dates<- as.Date(Project_data$Date, format="%m/%d/%y")
+# range(dates)
+# nrow(Project_data)
+# table(year(dates), months(dates))
+# names(sort(table(Project_data$PdDistrict), decreasing=TRUE))
+jghvbcns,
+# Project_data$Category <- factor(Project_data$Category, levels=names(sort(table(Project_data$Category), decreasing=TRUE)))
+# 
